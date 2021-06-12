c5f5ec70d65fd711beb1e13d2f4603b0a3b5cf66
Guang Lei <575403441@qq.com>
2021-06-12 18:54:02 +0800

    Initial Commit

---
+++ Rooms/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rooms
+//
+//  Created by Guang Lei on 2021/6/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rooms/RoomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomsApp.swift
+//  Rooms
+//
+//  Created by Guang Lei on 2021/6/12.
+//
+
+import SwiftUI
+
+@main
+struct RoomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
